ba2538e197cffc94d98c2b49a71409cbd6289c7f
Marcos Javier Magni Mattoni <javier.magni@gmail.com>
2023-02-03 20:45:50 +0100

    made exercises 1.3 to 1.5

--- part01/src/App.tsx
+++ part01/src/App.tsx
@@ -35,27 +35,29 @@ function Total({ parts }: { parts: IPart[] }) {
 }
 
 function App() {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div className="bg-cyan-100 flex flex-col items-center justify-center w-screen h-screen">
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 }
