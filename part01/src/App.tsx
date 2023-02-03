718f15a50fcf5b8509bf2585e3255d58cf26e197
Marcos Javier Magni Mattoni <javier.magni@gmail.com>
2023-02-03 14:21:05 +0100

    made exercises 1.1 and 1.2

--- part01/src/App.tsx
+++ part01/src/App.tsx
@@ -1,18 +1,62 @@
-import "./App.css";
+type IPart = {
+  name: string;
+  exercises: number;
+};
+
+function Header({ course }: { course: string }) {
+  return <h1 className="ml-2 text-xl font-bold">{course}</h1>;
+}
+
+function Content({ parts }: { parts: IPart[] }) {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.name} part={part} />
+      ))}
+    </div>
+  );
+}
+
+function Part({ part }: { part: IPart }) {
+  return (
+    <p className="m-2 text-lg">
+      {part.name} {part.exercises}
+    </p>
+  );
+}
+
+function Total({ parts }: { parts: IPart[] }) {
+  return (
+    <p className="m-2 text-lg">
+      Number of exercises{" "}
+      {parts.reduce((carry, part) => carry + part.exercises, 0)}
+    </p>
+  );
+}
 
 function App() {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
+  const course = "Half Stack application development";
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
-    <div className="bg-cyan-200">
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
-    </div>   
+    <div className="bg-cyan-100 flex flex-col items-center justify-center w-screen h-screen">
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
   );
 }
 
