e107a5ea10f27f9470bb1c6506d1b35e7236f41a
Marcos Javier Magni Mattoni <javier.magni@gmail.com>
2023-02-10 18:53:15 +0100

    exercise 1.13

--- part01/src/App.tsx
+++ part01/src/App.tsx
@@ -131,35 +131,45 @@ function Button({ text, onClick }: { text: string; onClick?: () => void }) {
 //   );
 // }
 
-
-
 function App() {
   const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-  const [selected, setSelected] = useState(0)
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+  const [selected, setSelected] = useState(0);
+
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
 
   function getRandomInt() {
     return Math.floor(Math.random() * anecdotes.length);
   }
 
   function updateSelected() {
-    setSelected(getRandomInt())
+    setSelected(getRandomInt());
+  }
+
+  function updateVotes() {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
   }
 
   return (
     <div className="bg-cyan-100 flex flex-col items-center w-screen h-screen">
       <p className="text-lg max-w-md">{anecdotes[selected]}</p>
-      <Button text="next anecdote" onClick={updateSelected} />
+      <p className="text-lg">has {votes[selected]} votes</p>
+      <div>
+        <Button text="vote" onClick={updateVotes} />
+        <Button text="next anecdote" onClick={updateSelected} />
+      </div>
     </div>
-  )
+  );
   // const [good, setGood] = useState(0);
   // const [neutral, setNeutral] = useState(0);
   // const [bad, setBad] = useState(0);
