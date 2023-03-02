dd5995ae8e9bfe6ee34dbf1e4c4c3496fe5ab13f
mjmmattoni98 <javier.magni@gmail.com>
2023-03-02 16:57:46 +0100

    exercise 3.6

--- part03/index.js
+++ part03/index.js
@@ -64,6 +64,12 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
   const person = {
     ...body,
     id: generateId(),
