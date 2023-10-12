0efd9b99569d5917ec9d14da103a4db3ac41c337
mjmmattoni98 <javier.magni@gmail.com>
2023-10-12 20:55:20 +0200

    fixed little bug

--- part03/index.js
+++ part03/index.js
@@ -19,22 +19,22 @@ let persons = [
   {
     id: 1,
     name: "Arto Hellas",
-    number: "040-123456",
+    phone: "040-123456",
   },
   {
     id: 2,
     name: "Ada Lovelace",
-    number: "39-44-5323523",
+    phone: "39-44-5323523",
   },
   {
     id: 3,
     name: "Dan Abramov",
-    number: "12-43-234345",
+    phone: "12-43-234345",
   },
   {
     id: 4,
     name: "Mary Poppendieck",
-    number: "39-23-6423122-3",
+    phone: "39-23-6423122-3",
   },
 ];
 
